115b6cd81eb86d610c3eb8da4d9ddb9f598966b9
avinashkotyada <2019ucs0089@iitjammu.ac.in>
2022-11-18 12:20:45 +0530

    h2 added

--- extractdata_xxxxxxx.py
+++ extractdata_xxxxxxx.py
@@ -2,31 +2,38 @@ import sqlite3
 import pandas as pd
 import json
 
-conn = sqlite3.connect('cma-artworks.db')
-conn.row_factory = sqlite3.Row
-c = conn.cursor()
+connect = sqlite3.connect('cma-artworks.db')
+connect.row_factory = sqlite3.Row
+c = connect.cursor()
 
 
-def table_info(c, conn):
-    rows1 = c.execute("SELECT * from artwork__department").fetchall()
-    rows2 = c.execute("SELECT * from artwork").fetchall()
-    rows3 = c.execute("SELECT * from creator").fetchall()
-    rows4 = c.execute("SELECT * from department").fetchall()
-    rows5 = c.execute("SELECT * from artwork__creator").fetchall()
-    jsonss = {
-        'artwork__department': json.dumps([dict(ix) for ix in rows1]),
-        'artwork': json.dumps([dict(ix) for ix in rows2]),
-        'creator': json.dumps([dict(ix) for ix in rows3]),
-        'department': json.dumps([dict(ix) for ix in rows4]),
-        'artwork__creator': json.dumps([dict(ix) for ix in rows5])
+def table_info(c, connect):
+    rows_1 = c.execute("SELECT * from artwork__department").fetchall()
+    rows_2 = c.execute("SELECT * from artwork").fetchall()
+    rows_3 = c.execute("SELECT * from creator").fetchall()
+    rows_4 = c.execute("SELECT * from department").fetchall()
+    rows_5 = c.execute("SELECT * from artwork__creator").fetchall()
+
+    h1= json.dumps([dict(ix) for ix in rows_1])
+    h2= json.dumps([dict(ix) for ix in rows_2])
+    h3= json.dumps([dict(ix) for ix in rows_3])
+    h4= json.dumps([dict(ix) for ix in rows_4])
+    h5=json.dumps([dict(ix) for ix in rows_5])
+    js = {
+        'artwork__department': h1,
+        'artwork': h2,
+        'creator': h3,
+        'department': h4,
+        'artwork__creator': h5
     }
-    print(jsonss)
-    conn.commit()
-    conn.close()
 
-    with open('artworks.txt', 'w') as f:
-        f.write(str(jsonss))
+    print(js)
+    connect.commit()
+    connect.close()
+
+    with open('artworks.txt', 'w') as p:
+        p.write(str(js))
 
 
 
-table_info(c, conn)
+table_info(c,connect)
--- h2.py
+++ h2.py
@@ -2,12 +2,11 @@ import numpy as np
 from scipy.optimize import linprog
 import random
 
-no_of_sensors = 5
+no_of_sensors = 100
 no_of_configurations = no_of_sensors
 maximum_capacity_of_sensors = 2000
 distance_between = [5, 1, 4, 4]
 prefix_distance = [0]
-prefix_distance_squares = []
 ratios = []
 energy = []
 rounds = []
@@ -18,57 +17,49 @@ rx = 2
 for i in range(no_of_sensors - 1):
     prefix_distance.append(prefix_distance[-1] + distance_between[i])
 
-for i in range(no_of_sensors):
-    prefix_distance_squares.append(prefix_distance[i] ** 2 + base_station_distance ** 2);
+allvals=[]
+for i in range(no_of_configurations):
+    vals=[]
+    for j in range(no_of_sensors):
+        val = 0
+        if i == j:
+            val = base_station_distance ** 2 + prefix_distance[i] ** 2 + 2 * rx
+        elif i < j:
+            val = distance_between[i] ** 2 + rx
+        else:
+            val = distance_between[i - 1] ** 2 + rx
+        if i != 0 and i != no_of_sensors - 1:
+            val += rx
+        vals.append(val)
+        print(val, end=" ")
+    allvals.append(vals)
+    ratios.append(max(vals)/sum(vals))
     energy.append(maximum_capacity_of_sensors)
     rounds.append(0)
+    print()
 
-sumvalue = sum(prefix_distance_squares)
+# print(ratios)
 
-for i in range(no_of_sensors):
-    ratios.append((((prefix_distance_squares[i]) / sumvalue)))
-
-sumvalue2 = sum(ratios)
-
+# sumvalue = sum(ratios)
 # for i in range(no_of_sensors):
-#     ratios[i] = (ratios[i]/sumvalue2)
+#     ratios[i] = ratios[i]/sumvalue
 
-print(sumvalue)
-print(sumvalue2)
 print(distance_between)
 print(prefix_distance)
-print(prefix_distance_squares)
 print(energy)
 print(ratios)
 print(rounds)
 
-
-for i in range(no_of_configurations):
-    for j in range(no_of_sensors):
-        val = 0
-        if i == j:
-            val = base_station_distance ** 2 + prefix_distance[j] ** 2 + 2 * rx
-        elif j < i:
-            val = distance_between[j] ** 2 + rx
-        else:
-            val = distance_between[j - 1] ** 2 + rx
-        if j != 0 and j != no_of_sensors - 1:
-            val += rx
-        print(val, end=" ")
-    print()
-
 print()
-print()
-
-for l in range(10):
+for l in range(2):
     for i in range(no_of_configurations):
         can_be = True
-        upto = energy[i] * ratios[i]
-        energyleft = energy[i] - upto
-        print(energyleft)
-        energy[i] = upto
-
-        while can_be:
+        upto = energy[i]/max(allvals[i])
+        can_be_rounds = upto*ratios[i]
+        print(can_be_rounds)
+        k=0
+        while(k<can_be_rounds and can_be):
+            k=k+1
             for j in range(no_of_sensors):
                 val = 0
                 if i == j:
@@ -84,7 +75,6 @@ for l in range(10):
                     can_be = False
                     break
 
-
             if can_be:
                 for j in range(no_of_sensors):
                     val = 0
@@ -99,15 +89,13 @@ for l in range(10):
 
                     energy[j] = energy[j] - val
 
-
                 rounds[i] = rounds[i] + 1
             else:
                 break
-        print(energy)
-        energy[i] = energy[i] + energyleft
         print(rounds)
         print(energy)
         print()
+
     print()
     print()
     print()
